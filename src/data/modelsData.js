ebca14de537e4ac0db09d94ea3f8a3943c57f410
Oken Keithellakpam <keithellakpamoken@gmail.com>
2025-05-31 14:17:00 +0530

    Added new models and removed old ones

--- src/data/modelsData.js
+++ src/data/modelsData.js
@@ -1,8 +1,11 @@
 export const models = [
+    { id: "deepseek/deepseek-r1-0528-qwen3-8b:free", name: 'DeepSeek R1 0528 Qwen 8B', decription: "Fine tuned Quen 8b with DeepSeek R1 0528"},
     { id: 'deepseek/deepseek-chat-v3-0324:free', name: 'DeepSeek Chat V3', description: 'Conversational model - recommended' },
-    { id: 'deepseek/deepseek-r1-zero:free', name: 'DeepSeek R1-Zero', description: 'Advanced reasoning model' },
+    { id: 'deepseek/deepseek-r1-0528:free', name: 'DeepSeek R1-Zero', description: 'Advanced reasoning model' },
     { id: 'deepseek/deepseek-prover-v2:free', name: 'DeepSeek Prover', description: 'Mathematical reasoning focused' },
     { id: 'qwen/qwen3-235b-a22b:free', name: 'Qwen3 235B', description: 'Wide-context reasoning model' },
     { id: 'tngtech/deepseek-r1t-chimera:free', name: 'DeepSeek Chimera', description: 'Multimodal analysis model' },
-    { id: 'agentica-org/deepcoder-14b-preview:free', name: 'DeepCoder 14B', description: 'Coding specialist (preview)' }
+    { id: 'agentica-org/deepcoder-14b-preview:free', name: 'DeepCoder 14B', description: 'Coding specialist (preview)' },
+    { id: "meta-llama/llama-4-maverick:free", name: "Meta LLama 4 Maverick", description: "Meta new latest model"},
+    { id: "meta-llama/llama-3.3-8b-instruct:free", name: "Meta LLama 3.3 8B Instruct", description: "Meta new llama 3.3"}
   ];
