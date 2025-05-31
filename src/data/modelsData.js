546fb84246ccd4dc47857225eaefb8c526e43648
Oken Keithellakpam <keithellakpamoken@gmail.com>
2025-05-31 14:40:57 +0530

    Remove and replace with a working model

--- src/data/modelsData.js
+++ src/data/modelsData.js
@@ -1,7 +1,6 @@
 export const models = [
     { id: "deepseek/deepseek-r1-0528-qwen3-8b:free", name: 'DeepSeek R1 0528 Qwen 8B', decription: "Fine tuned Quen 8b with DeepSeek R1 0528"},
-    { id: 'deepseek/deepseek-chat-v3-0324:free', name: 'DeepSeek Chat V3', description: 'Conversational model - recommended' },
-    { id: 'deepseek/deepseek-r1-0528:free', name: 'DeepSeek R1-Zero', description: 'Advanced reasoning model' },
+    { id: 'deepseek/deepseek-r1-0528:free', name: 'DeepSeek R1-0528', description: 'Advanced reasoning model' },
     { id: 'deepseek/deepseek-prover-v2:free', name: 'DeepSeek Prover', description: 'Mathematical reasoning focused' },
     { id: 'qwen/qwen3-235b-a22b:free', name: 'Qwen3 235B', description: 'Wide-context reasoning model' },
     { id: 'tngtech/deepseek-r1t-chimera:free', name: 'DeepSeek Chimera', description: 'Multimodal analysis model' },
