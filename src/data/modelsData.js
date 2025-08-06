1d3d7675563cb9399e0a3878f1cbabb5b2e03c84
OkenHaha <keithellakpamoken@gmail.com>
2025-08-06 14:36:37 +0530

    updated models

--- src/data/modelsData.js
+++ src/data/modelsData.js
@@ -1,10 +1,5 @@
 export const models = [
-    { id: "deepseek/deepseek-r1-0528-qwen3-8b:free", name: 'DeepSeek R1 0528 Qwen 8B', decription: "Fine tuned Quen 8b with DeepSeek R1 0528"},
-    { id: 'deepseek/deepseek-r1-0528:free', name: 'DeepSeek R1-0528', description: 'Advanced reasoning model' },
-    { id: 'deepseek/deepseek-chat-v3-0324:free', name: 'DeepSeek V3-0324', description: 'Mathematical reasoning focused' },
-    { id: 'qwen/qwen3-235b-a22b:free', name: 'Qwen3 235B', description: 'Wide-context reasoning model' },
-    { id: 'tngtech/deepseek-r1t-chimera:free', name: 'DeepSeek Chimera', description: 'Multimodal analysis model' },
-    { id: 'agentica-org/deepcoder-14b-preview:free', name: 'DeepCoder 14B', description: 'Coding specialist (preview)' },
-    { id: "meta-llama/llama-4-maverick:free", name: "Meta LLama 4 Maverick", description: "Meta new latest model"},
-    { id: "meta-llama/llama-3.3-8b-instruct:free", name: "Meta LLama 3.3 8B Instruct", description: "Meta new llama 3.3"}
+    {id: 'deepseek/deepseek-chat-v3-0324:free', name: 'DeepSeek V3-0324', description: 'Mathematical reasoning focused' },
+    {id: 'moonshotai/kimi-k2:free', name: 'Kimi K2', description: 'Agentic AI by Moonshot'},
+    {id: 'z-ai/glm-4.5-air:free', name: 'GLM 4.5 AIR', description: 'SOTA Agentic AI by Z.ai'}
   ];
